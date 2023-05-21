a1dcfc083ac30be835e383cd0852f9f9216c6c43
Afra <propazaman12@gmail.com>
2023-05-21 10:26:37 +0600

    read operations on categories by id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5002;
@@ -35,6 +35,14 @@ async function run() {
     app.get('/categories', async (req, res) => {
         const cursor = categoriesCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/categories/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const result = await categoriesCollection.findOne(query);
         res.send(result);
     })
 
