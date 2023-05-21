e6c4bd0a4018a927611c1f28d50fbfed2181fecd
Afra <propazaman12@gmail.com>
2023-05-21 23:27:52 +0600

    patch and delete method added

--- index.js
+++ index.js
@@ -74,6 +74,27 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/allToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedAddedToy = req.body;
+      console.log(updatedAddedToy);
+      const updateDoc = {
+          $set: {
+              status: updatedAddedToy.status
+          },
+      };
+      const result = await allToysCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  })
+
+  app.delete('/allToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await allToysCollection.deleteOne(query);
+      res.send(result);
+  })
+
 
 
     // Send a ping to confirm a successful connection
